1c09deda60063f445f95f2a4869bdf2b2621f375
Christian Micklisch <cmicklis@stetson.edu>
2015-09-14 12:04:08 -0400

    Implemented a workaround to an issue show here: https://jira.mongodb.org/browse/PHP-854
    The workaround to the issue was posted on Nov 14 2013 07:21:18 PM GMT+0000 by Hannes Magnusson.

--- src/FuelMonga.php
+++ src/FuelMonga.php
@@ -6,6 +6,9 @@ use \League\Monga;
 
 class FuelMonga
 {
+	const MAX_CONNECTION_RETRY_COUNT = 3;
+	const CANNOT_CONNECT_EXCEPTION = "Cannot connect to the MongoClient currently. Please check that your system is running.";
+
 	protected static $_instance;
 	protected static $_defaultDSN;
 	protected static $_defaultOptions;
@@ -48,8 +51,11 @@ class FuelMonga
 			if (isset($config['dsn'])) {
 				$dsn = $config['dsn'];
 			}
-
-			static::$_instance = Monga::connection($dsn, $options);
+			try {
+				static::$_instance = static::get_connection($dsn, $options);
+			} catch (\Exception $e) {
+				\Log::error('Exception thrown: (' . __FILE__ . '#' . __LINE__ . '): ' . $e->getMessage());
+			}
 		}
 		return static::$_instance;
 	}
@@ -79,6 +85,38 @@ class FuelMonga
 
 		if (!isset(static::$defaultOptions) && array_key_exists('options', $config)) {
 			static::$_defaultOptions = $config['options'];
+		}
+	}
+
+	/**
+	 * Goes and try's to create a connection to the MongoClient through the Monga Connection class. If a connection
+	 * cannot be made it tries to attempt a new connection until it has reached its maximum retry's. If this is the
+	 * case the function throws an exception stating that it cannot currently connect to the MongoDB Client. This code
+	 * is based off of an issue in the PHP Legacy driver, issue PHP-854. The URL to that issue is:
+	 *
+	 * 		https://jira.mongodb.org/browse/PHP-854
+	 *
+	 * The workaround to the issue was posted on Nov 14 2013 07:21:18 PM GMT+0000 by Hannes Magnusson.
+	 *
+	 * @param 	string 			$dsn		The Information for what mongo client(s) to connect to.
+	 * @param   array 			$options    All of the options for creating the MongoClient
+	 * @param   int				$retry		How many times we should try and retry creating a connection
+	 * @return  \Monga\Connection			The connection for the Monga object.
+	 * @throws \Exception
+	 */
+	protected static function get_connection($dsn, $options, $retry = self::MAX_CONNECTION_RETRY_COUNT)
+	{
+		try {
+			$instance = Monga::connection($dsn, $options);
+			return $instance;
+		} catch (\Exception $e) {
+			\Log::error('Exception thrown: (' . __FILE__ . '#' . __LINE__ . '): ' . $e->getMessage());
+		}
+
+		if ($retry > 0) {
+			return static::get_connection($dsn, $options, $retry);
+		} else {
+			throw new \Exception(self::CANNOT_CONNECT_EXCEPTION);
 		}
 	}
 }
